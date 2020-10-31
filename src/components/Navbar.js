c46272ed7e2c6edef1e70eb0ce6ea9d5166c8753
GHASSEN <ghassenbenslimane00@gmail.com>
2020-10-31 20:29:49 +0100

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../Css/Navbar.css";
-import {logo} from "../img/logo.png";
+
 
 
 export default class Navbar extends Component {
@@ -10,32 +10,33 @@ export default class Navbar extends Component {
   <nav className="navbar shadow  bg-white rounded fixed-top navbar-expand-lg navbar-light background-color:#6e6e6e">
   
   
-  <a className="navbar-brand"></a>
-  <div className="collapse navbar-collapse" id="navbarText">
+  
+  <div className="collapse navbar-collapse pt-0 pb-0 mt-0 mb-0" id="navbarText">
+    <a className="navbar-brand pb-2 mb-2"></a>
     <ul className="navbar-nav mr-auto">
-      <li className="nav-item rounded ">
+      <li className="nav-item rounded-top pb-2 ">
         <a className="nav-link" href="#"> Testez-Vous </a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Auto-Surveillance</a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Laboratoires</a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Covid-19?</a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Protégez-vous</a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Dons</a>
       </li>
-      <li className="nav-item rounded">
+      <li className="nav-item rounded-top pb-2">
         <a className="nav-link" href="#">Contactez-nous</a>
       </li>
     </ul>
-    <button class="btn btn-outline-success my-2 my-sm-0" >Connectez-vous!</button>
+    <button class="btn btn-outline-success  mb-2" >Connectez-vous !</button>
    
   </div>
 </nav>
