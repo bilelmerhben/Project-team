5bd8e183987e9aea5ad72f2190afd1c83904b3c8
GHASSEN <ghassenbenslimane00@gmail.com>
2020-10-31 16:08:35 +0100

    ghassen

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React from "react";
 import Carousel from 'react-elastic-carousel';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Card from "./components/slider";
+
 import Navbar from "./components/Navbar";
 import Box from "./components/Box";
 
 import "./Css/App.css";
 
 function App() {
-  const breakPoints =[
-    {width : 1300 , itemsToShow:1},
-];
+  
   return (
     
     <div className="App">
 
       <Navbar/>
-      <Carousel class="cars" breakPoints={breakPoints}>
-            <Card src ="/img2.jpg"/>
-            <Card src ="/img3.png"/>
-            <Card src ="/img1.jpg"/>
-           
       
-      </Carousel>
   
       <Box/>
       
 
 
 
-      <footer>Footer</footer>
+     
     </div>
   );
 }
