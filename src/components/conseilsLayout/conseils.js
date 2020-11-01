f026076d704d6835ac8489f9a696aa1b18e0cc91
GHASSEN <ghassenbenslimane00@gmail.com>
2020-11-01 23:43:37 +0100

    .

--- src/components/conseilsLayout/conseils.js
+++ src/components/conseilsLayout/conseils.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./container"
-function conseils(title="",image="",container=""){
+function conseils(title,image,container){
     return(
         <div>
             <h1>{title}</h1>
