25a914be0d233f5899c0c0ad5562dbc6eead87ff
bilelmerhben <merhbenbilel91@gmail.com>
2020-11-10 20:30:38 +0100

    animation

--- src/components/Container1.js
+++ src/components/Container1.js
@@ -8,16 +8,16 @@ function Container1 ({show,close}){
     return (
        
  <div 
-     className="contain1 d-flex justify-content-around"
+     className="contain1  d-flex justify-content-around"
      style ={{
       opacity : show ? '1' :'0'
             }}
  >
         
        
-        <Testbox classname={"bo1 p-2 bd-highlight"}  title={"TEST1"} />
-        <Testbox classname={"bo2 p-2 bd-highlight"}  title={"TEST2"} />
-        <Testbox classname={"bo3 p-2 bd-highlight"} top={"/Laboratoires"} title={"Laboratoires les plus proches"} />
+        <Testbox classname={"bo1 color-change-5x p-2 bd-highlight"}  title={"TEST1"} />
+        <Testbox classname={"bo2 color-change-5x p-2 bd-highlight"}  title={"TEST2"} />
+        <Testbox classname={"bo3 color-change-5x p-2 bd-highlight"} top={"/Laboratoires"} title={"Laboratoires les plus proches"} />
         <button  onClick={close} type="button" class="btn btn-info bt1 p-2 bd-highlight ">Close</button>
 
      
