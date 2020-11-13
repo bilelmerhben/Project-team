99fd33b3044074057f1eff104808377717d28cc3
bilelmerhben <merhbenbilel91@gmail.com>
2020-11-13 15:15:17 +0100

    link

--- src/components/testBtnLayout/Box.js
+++ src/components/testBtnLayout/Box.js
@@ -1,16 +1,22 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "../../Css/box.css";
 
-export default function Box({ title, classname, img , text }) {
+export default function Box({ title, classname, img , text,lien }) {
 
   return (
-
+<Link to={lien}>
     <div className={classname}>
-      <img src={img} alt={""} />
-      <h4 classname={classname}>{title}</h4>
-  <p>{text}</p>
-    </div>
+       <img src={img} alt={""} />
 
+      <h4 classname={classname}>
+       {title}
+      </h4>
+
+      <p>{text}</p>
+
+    </div>
+    </Link>
   )
 }
 
--- src/components/testBtnLayout/buttons.js
+++ src/components/testBtnLayout/buttons.js
@@ -8,13 +8,13 @@ class Btns extends Component{
         <div className="container text-center box1">
             <div className="row justify-content-md-center mx-0">
                 <div className="col px-0 text-center">
-        <Box classname={"sub1"} img={test} title={"TEST"} text={"Some text "}/>
+        <Box classname={"sub1"} img={test} title={"TEST 1"} text={"Some text "} lien={"/Test1"}/>
         </div>
         <div className="col px-0 text-center">
-        <Box classname={"sub2"} img={test} title={"TEST"} text={"Some text "}/>
+        <Box classname={"sub2"} img={test} title={"TEST 2"} text={"Some text "} lien={"/Test2"} />
         </div >
         <div className="col px-0 text-center">
-        <Box classname={"sub3"} img={test} title={"TEST"} text={"Some text "}/>
+        <Box classname={"sub3"} img={test} title={"Labos plus Proche"} text={"Some text "} lien={"/Laboratoires"}/>
         </div>
       
 
