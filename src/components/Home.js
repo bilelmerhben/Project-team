754871b44aa945b9ac8924faba44f138b4d9e0d0
bilelmerhben <53703326+bilelmerhben@users.noreply.github.com>
2020-12-20 13:13:14 +0100

    layer3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Btns from "./testBtnLayout/buttons";
 import "../Css/Home.css";
+import conseil from "../img/conseil.png";
+import icon1cons from "../img/icon1cons.png";
 
 function Home() {
   return (
@@ -28,6 +30,19 @@ function Home() {
         <h5>Fièvre, fatigue, maux de tête, toux et maux de gorge, courbatures, gêne respiratoire.
          Pour vérifier si un médicament présente un risque d’aggraver vos symptômes :</h5>
          <button>Covid19 - medicaments.com</button>
+    </div>
+    <div className="thirdLayerHome">
+      <img src={conseil} href="conseil" style={{width:300,height:100}}/>
+      
+        <h1>Comment se protéger et protéger les autres au quotidien ?</h1>
+        <img src={icon1cons} href="" />
+       <h5>Respectez au maximum les mesures de distanciation sociales.</h5>
+       <h5>Portez un masque.</h5>
+       <h5>Limitez les interactions sociales, certaines personnes transmettent le virus sans en ressentir les symptômes.</h5>
+       <h5>Lavez-vous très régulièrement les mains.</h5>
+       <h5>Toussez ou éternuez dans votre coude ou dans un mouchoir.</h5>
+       <h5>Saluez sans serrer la main, n'embrassez personne.</h5>
+       <h5>Utilisez un mouchoir à usage unique et jetez-le.</h5>
     </div>
     </div>
     
