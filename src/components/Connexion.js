911d067c341dae7b0d23634c06415fd25c431301
mariemnasri <49901409+mariemnasri@users.noreply.github.com>
2020-12-25 02:37:04 +0100

    a

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -12,135 +12,135 @@ class Connexion extends Component {
       email: "",
       password: "",
       errors: {},
-      submitUser : true
+      submitUser: true,
     };
   }
   componentWillReceiveProps(nextProps) {
-
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/"); 
+      this.props.history.push("/");
     }
-if (nextProps.errors) {
+    if (nextProps.errors) {
       this.setState({
-        errors: nextProps.errors
+        errors: nextProps.errors,
       });
     }
   }
 
-
-
-onChange = e => {
+  onChange = (e) => {
     this.setState({ [e.target.id]: e.target.value });
   };
-onSubmit = e => {
+  onSubmit = (e) => {
     e.preventDefault();
-const userData = {
+    const userData = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
     };
     this.props.loginUser(userData);
   };
-  onSubmitLabo = e => {
+  onSubmitLabo = (e) => {
     e.preventDefault();
-const userLabo = {
+    const userLabo = {
       email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
     };
     this.props.loginLabo(userLabo);
   };
 
-
-  render(){
+  render() {
     const { errors } = this.state;
 
-  return (
-    <div className="login-card">
-      <div className="login-content">
-  <h1>Connectez-vous </h1>
-      <form noValidate onSubmit={(e)=>{
-        if (this.state.submitUser)
-        this.onSubmit(e)
-        else this.onSubmitLabo(e)
-      }}>
-      <hr/>
-      <div className="form-group ">
-        <div className="row">
-          <div className="col">
-          <input 
-          type="radio" 
-          value="None" 
-          id="radioOne" 
-          name="account"
-          onClick={() => this.setState({submitUser:true})}  />
-          <label htmlFor="radioOne" className="radio1" >Patient</label> </div>
-          <div className="col">
-          <input 
-          type="radio" 
-          value="None" 
-          id="radioTwo" 
-          name="account" 
-          onClick={() =>this.setState({submitUser:false})} />
-          <label htmlFor="radioTwo" className="radio1">Laboratoire</label></div>
+    return (
+      <div className="login-card">
+        <div className="login-content">
+          <h1>Connectez-vous </h1>
+          <form
+            noValidate
+            onSubmit={(e) => {
+              if (this.state.submitUser) this.onSubmit(e);
+              else this.onSubmitLabo(e);
+            }}
+          >
+            <hr />
+            <div className="form-group ">
+              <div className="row">
+                <div className="col">
+                  <input
+                    type="radio"
+                    value="None"
+                    id="radioOne"
+                    name="account"
+                    onClick={() => this.setState({ submitUser: true })}
+                  />
+                  <label htmlFor="radioOne" className="radio1">
+                    Patient
+                  </label>{" "}
+                </div>
+                <div className="col">
+                  <input
+                    type="radio"
+                    value="None"
+                    id="radioTwo"
+                    name="account"
+                    onClick={() => this.setState({ submitUser: false })}
+                  />
+                  <label htmlFor="radioTwo" className="radio1">
+                    Laboratoire
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="form-group margin-input">
+              <label htmlFor="InputEmail1">Adresse Email</label>
+              <input
+                type="email"
+                className={classnames("form-control", {
+                  "is-invalid": errors.email,
+                })}
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Entrer email"
+                onChange={this.onChange}
+                value={this.state.email}
+              />
+              <p className="text-danger">{errors.email} </p>
+              <p className="text-danger">{} </p>
+            </div>
+            <div className="form-group margin-input">
+              <label htmlFor="exampleInputPassword1">Mot de passe</label>
+              <input
+                type="password"
+                className={classnames("form-control", {
+                  "is-invalid": errors.password,
+                })}
+                id="password"
+                placeholder="Mot de passe"
+                onChange={this.onChange}
+                value={this.state.password}
+              />
+              <p className="text-danger">{errors.password} </p>
+            </div>
+
+            <button type="submit" className="connecter-1">
+              Valider
+            </button>
+            <div className="form-group pt-2">
+              <a type="button" className="inscription-1" href="/Inscription">
+                S'inscrire
+              </a>
+            </div>
+          </form>
         </div>
       </div>
-        <div className="form-group margin-input">
-        <label htmlFor="InputEmail1">Adresse Email</label>
-          <input
-            type="email"
-            className={classnames("form-control", {
-        "is-invalid": errors.email
-      })} 
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Entrer email"
-            onChange={this.onChange}
-            value={this.state.email}
-          />
-                      <p className="text-danger">{errors.email} </p>
-                      <p className="text-danger">{} </p>
-
-        </div>
-        <div className="form-group margin-input">
-          <label htmlFor="exampleInputPassword1">Mot de passe</label>
-          <input
-            type="password"
-            className={classnames("form-control", {
-        "is-invalid": errors.password
-      })} 
-            id="password"
-            placeholder="Mot de passe"
-            onChange={this.onChange}
-            value={this.state.password}
-
-          />
-                      <p className="text-danger">{errors.password} </p>
-
-        </div>
-        
-        <button type="submit" className="connecter-1">
-          Valider
-        </button>
-        <div className="form-group pt-2">
-          <a type="button" className="inscription-1" href="/Inscription">
-            S'inscrire
-          </a>
-        </div>
-      </form>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 Connexion.propTypes = {
   loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
 };
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  errors: state.errors
+  errors: state.errors,
 });
-export default connect(
-  mapStateToProps,
-  { loginUser , loginLabo}
-)(Connexion);
+export default connect(mapStateToProps, { loginUser, loginLabo })(Connexion);
